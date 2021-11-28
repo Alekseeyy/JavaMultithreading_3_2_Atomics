5a2afe281015cc33bbe4ffcc4042510ff1ff3bf6
Aleksey Falev <aleksej.falev@mail.ru>
2021-11-28 16:33:59 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static final long SIZE = 20;
+    private static final int LOW_BOUND = 10;
+    private static final int HIGH_BOUND = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        LongAdder stat = new LongAdder();
+        random.ints(SIZE, LOW_BOUND, HIGH_BOUND).boxed().collect(Collectors.toList())
+                .forEach(i -> executorService.submit(() -> stat.add(i)));
+        random.ints(SIZE, LOW_BOUND, HIGH_BOUND).boxed().collect(Collectors.toList())
+                .forEach(i -> executorService.submit(() -> stat.add(i)));
+        random.ints(SIZE, LOW_BOUND, HIGH_BOUND).boxed().collect(Collectors.toList())
+                .forEach(i -> executorService.submit(() -> stat.add(i)));
+        executorService.awaitTermination(3, TimeUnit.SECONDS);
+        System.out.println("\nРезультат: " + stat.sum());
+        executorService.shutdown();
+    }
+}
